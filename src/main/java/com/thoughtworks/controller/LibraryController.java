e072aa3d24d6acfbe23b9885c1fbd7e7109b120d
Swapnil Gaikwad <swapnilg@thoughtworks.com>
2017-02-11 15:03:51 +0530

    Tried PG as DB.

--- src/main/java/com/thoughtworks/controller/LibraryController.java
+++ src/main/java/com/thoughtworks/controller/LibraryController.java
@@ -1,31 +1,15 @@
 package com.thoughtworks.controller;
 
 import com.thoughtworks.model.Book.Book;
-import com.thoughtworks.repository.BookRepository;
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.*;
 
 @RestController
 @RequestMapping( value = "/" )
 public class LibraryController {
 
-    private BookRepository bookRepository;
-
-    @Autowired
-    public LibraryController(BookRepository bookRepository) {
-        this.bookRepository = bookRepository;
-    }
-
     @RequestMapping( method = RequestMethod.GET )
     public @ResponseBody Book getBooks( @ModelAttribute Book book ) {
-        return bookRepository.findOne( book.getId() );
-    }
-
-    @RequestMapping( method = RequestMethod.POST )
-    public @ResponseBody boolean saveBook( @ModelAttribute Book book ) {
-        Book savedBook = bookRepository.save( book );
-        System.out.println( savedBook );
-        return savedBook != null;
+        return book;
     }
 
 }
--- src/main/java/com/thoughtworks/model/Book/Book.java
+++ src/main/java/com/thoughtworks/model/Book/Book.java
@@ -1,12 +1,7 @@
 package com.thoughtworks.model.Book;
 
-import org.springframework.data.annotation.Id;
-import org.springframework.data.mongodb.core.mapping.Document;
-
-@Document
 public class Book {
 
-    @Id
     private String id;
     private String title;
 
